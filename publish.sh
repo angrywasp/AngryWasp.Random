c28cb1546fe3317b6048fd16b11fe44591285416
angrywasp <angrywasp@live.com>
2024-09-18 19:13:57 +1000

    Update to net8

--- publish.sh
+++ publish.sh
@@ -25,4 +25,4 @@ function getNewVersion()
 version=$(getNewVersion $1 patch)
 
 dotnet build -c Release Src/AngryWasp.Random.sln -p:PackageVersion=${version}
-dotnet nuget push Bin/Binaries/AngryWasp.Random.${version}.nupkg --source https://api.nuget.org/v3/index.json
+dotnet nuget push Bin/Binaries/AngryWasp.Random.${version}.nupkg --source https://api.nuget.org/v3/index.json --api-key ${NUGET_API_KEY}
